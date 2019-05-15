42957ff0271ead60be0f2f06e092a4d4d0a210b5
lightswarm124 <lightswarm@gmail.com>
2019-05-15 12:01:22 -0400

    localhost mongodb

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-//  mongoURI: "mongodb://bitcoinbay:cryptopos@cluster0-ycogn.mongodb.net/test?retryWrites=true",
-  mongoURI: "mongodb://localhost:27017/cryptoPoS",
+  mongoURI: "mongodb://bitcoinbay:cryptopos@cluster0-ycogn.mongodb.net/test?retryWrites=true",
+//  mongoURI: "mongodb://localhost:27017/cryptoPoS",
   secretOrKey: "secret"
 };
