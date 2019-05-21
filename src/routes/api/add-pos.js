e45c1c104faaed846459ccaed4a1a1fc6268db07
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-05-21 17:16:50 -0400

    Add api route for adding PoS'
    This route will be called with a PoS object to be stored in the database.

---
+++ src/routes/api/add-pos.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const PoS = require("../../models/PoS");
+
+router.post("/add-pos/:new_pos", wrap(async (req, res) => {
+    
+}));
