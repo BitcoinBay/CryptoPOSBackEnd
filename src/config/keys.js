2957bd05de078d9164a394ef3aabe7cdaf25ce0e
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-05-29 13:08:09 -0400

    testing

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://lightswarm:Qiancheng_124@bitcoin-bay-ummyy.mongodb.net/test?retryWrites=true",
-//  mongoURI: "mongodb://localhost:27017/cryptoPoS",
+  // mongoURI: "mongodb+srv://lightswarm:Qiancheng_124@bitcoin-bay-ummyy.mongodb.net/test?retryWrites=true",
+  mongoURI: "mongodb://localhost:27017/cryptoPoS",
   secretOrKey: "secret"
 };
