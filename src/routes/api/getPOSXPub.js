e694b1cce4ef06fcab6483ac0480d8ddd509aa37
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-05-30 13:42:45 -0400

    Remove debug console log

--- src/routes/api/getPOSXPub.js
+++ src/routes/api/getPOSXPub.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const PoS = require("../../models/PoS");
 
 router.post("/", (req, res) => {
-    console.log(req.body.pos_id);
-
     PoS.findById(req.body.pos_id).populate("xpub").exec((error, pos) => {
         if (error) {
             res.status(400).json(error);
