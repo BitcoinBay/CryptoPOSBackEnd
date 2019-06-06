ef8d0aca452cd8d53d1952acd8151ec59a7f3b70
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-06-06 13:49:15 -0400

    Fix population of xPubs for multiple

--- src/routes/api/getPOSByID.js
+++ src/routes/api/getPOSByID.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const PoS = require("../../models/PoS");
 
 router.post("/", (req, res) => {
-    PoS.findById(req.body.pos_id).populate("xpub").exec((error, pos) => {
+    PoS.findById(req.body.pos_id).populate("xpubs").exec((error, pos) => {
         if (error) {
             res.status(400).json(error);
         } else {
