f65fe5b29db66f072546f508a3ad58e56ab87ba4
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-06-24 15:57:15 -0400

    Fix formatting

--- src/routes/api/addXPub.js
+++ src/routes/api/addXPub.js
@@ -8,24 +8,22 @@ const PoS = require("../../models/PoS");
 
 router.post("/", (req, res) => {
     const new_xpub = new XPub({
-      address: req.body.address,
-      type: req.body.type,
-      address_index: 0
+        address: req.body.address,
+        type: req.body.type,
+        address_index: 0
     });
 
     if (new_xpub.address.length === 111) {
-      new_xpub.save().then((saved_xpub) => {
-          PoS.findByIdAndUpdate(req.body.pos_id, { $push: { xpubs: saved_xpub } })
-              .exec((error, pos) => {
-                  if (!error) {
-                      res.json(saved_xpub);
-                  }
-              }).catch((error) => {
-                  console.log(error);
-              });
-      }).catch((error) => {
-          console.log(error);
-      });
+        new_xpub.save().then((saved_xpub) => {
+            PoS.findByIdAndUpdate(req.body.pos_id, { $push: { xpubs: saved_xpub } })
+                .exec((error, pos) => {
+                    if (!error) {
+                        res.json(saved_xpub);
+                    }
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
     } else {
         res.status(400).end();
     }
