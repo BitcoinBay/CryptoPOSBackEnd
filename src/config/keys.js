367e15bc73d69da38414b35d6dae41f037bc22da
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-07-18 16:12:04 -0400

    Take out placeholder JWT key

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
   // mongoURI: "mongodb+srv://lightswarm:Qiancheng_124@bitcoin-bay-ummyy.mongodb.net/test?retryWrites=true",
   mongoURI: "mongodb://localhost:27017/cryptoPoS",
-  secretOrKey: "secret"
 };
