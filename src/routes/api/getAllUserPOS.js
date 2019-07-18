ba4eb5bbec5a117be8c343c42064f798b5821897
Lars Kloosterman <lars_kloosterman@outlook.com>
2019-07-18 16:17:18 -0400

    Use new JWT token auth

--- src/routes/api/getAllUserPOS.js
+++ src/routes/api/getAllUserPOS.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+import { checkJWTToken } from "../index";
 
 const User = require("../../models/User");
 
-router.post("/", (req, res) => {
-    User.findById(req.body.user_id).populate("pos_systems").exec((error, user) => {
-        if (!error && user) {
-            res.json(user.pos_systems);
+router.post("/", checkJWTToken, (req, res) => {
+    jwt.verify(req.token, process.env.JWT_KEY, (error, user_data) => {
+        if (error) {
+            console.log(error);
+
+            res.status(403).end();
         } else {
-            res.status(400).json(error);
+            User.findById(user_data.id).populate("pos_systems").exec((error, user) => {
+                if (!error && user) {
+                    res.json(user.pos_systems);
+                } else {
+                    res.status(400).json(error);
+                }
+            });
         }
-    });
+    })
 });
 
 
